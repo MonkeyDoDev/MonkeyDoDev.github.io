eb45181e638b52ed518d066b31a04034ad0eb188
MonkeyDoDev <125860874+MonkeyDoDev@users.noreply.github.com>
2023-08-29 15:48:20 +0100

    ffs1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "980547175e325fe622a3362b84d55b6a",
-"index.html": "26c048513690d829a839c64928188e1d",
-"/": "26c048513690d829a839c64928188e1d",
+"index.html": "f10b5fa33a10ffa00cdfc1da16cd9f90",
+"/": "f10b5fa33a10ffa00cdfc1da16cd9f90",
 "CNAME": "2a8e3685c342b57810f80c780dc60bbf",
 "main.dart.js": "a85e663234e886e50685b0e68cc54e82",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
